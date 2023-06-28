5ee906c2f4586d676c1fba422c1ba6f93f151cee
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-06-28 23:18:44 +0200

    Add files via upload

---
+++ Page.tsx
@@ -0,0 +1,22 @@
+import "./styles.css"
+
+export default function Page() {
+  return (
+    <div>
+      <div class="bar">
+        <p class="title">Bar</p>
+            </div>
+      <div class="bar">
+          <button class="barbut">First Page</button>
+          <button class="barbut">Second Page</button>
+          <button class="barbut">Third Page</button>
+         </div>  
+    
+      
+    <div>
+
+    </div>
+      
+    </div>
+  )
+}
